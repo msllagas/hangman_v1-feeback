fdc18097cfae2aed5fc113b786f4db6b2021a371
William <blazerx1234@gmail.com>
2022-12-08 05:01:09 +0800

    fuzzy update

--- Hangman/Assets/Scripts/GameManager.cs
+++ Hangman/Assets/Scripts/GameManager.cs
@@ -120,7 +120,7 @@ public class GameManager : MonoBehaviour
             if (currentMistakes == maxMistakes) // 32
             {
                 // Debug.Log("Lost Game");
-                UIHandler.instance.LoseCondition(playTime, currentMistakes, maxHints); // 38
+                UIHandler.instance.LoseCondition(playTime, maxHints); // 38
                 gameOver = true;
                 
                 return;
@@ -134,7 +134,7 @@ public class GameManager : MonoBehaviour
         if (gameOver) // 25
         {
             // show ui
-            UIHandler.instance.WinCondition(playTime, currentMistakes, maxHints);
+            UIHandler.instance.WinCondition(playTime, maxHints);
         }
     }
 
