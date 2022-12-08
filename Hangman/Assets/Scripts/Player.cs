e9c2ef41b82bf833b20ce2e75432c2a597771b50
William <blazerx1234@gmail.com>
2022-12-08 21:30:56 +0800

    rdb fix

--- Hangman/Assets/Scripts/Player.cs
+++ Hangman/Assets/Scripts/Player.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 
 public class Player
 {
-   /* public int totalWins;
-    public int totalLosses;
-
-    public int gamesPlayed;
-    public float winRatio;*/
-
+  
     public float motivationLevel;
     public float averageMotivationLevel;
+    public float motLevPerc;
+    public float aveMLPerc;
 
-/*    public Player(int totalWins, int totalLosses, int gamesPlayed, float winRatio)
-    {
-        this.totalWins = totalWins;
-        this.totalLosses = totalLosses;
-        this.gamesPlayed = gamesPlayed;
-        this.winRatio = winRatio;
-    }*/
+    /*    public Player(int totalWins, int totalLosses, int gamesPlayed, float winRatio)
+        {
+            this.totalWins = totalWins;
+            this.totalLosses = totalLosses;
+            this.gamesPlayed = gamesPlayed;
+            this.winRatio = winRatio;
+        }*/
 
-    public Player(float motivationLevel, float averageMotivationLevel )
+    public Player(float motivationLevel, float averageMotivationLevel, float motLevPerc, float aveMLPerc)
     {
-       this.motivationLevel = motivationLevel;
+        this.motivationLevel = motivationLevel;
         this.averageMotivationLevel = averageMotivationLevel;
+        this.motLevPerc = motLevPerc;
+        this.aveMLPerc = aveMLPerc;
     }
 
 
