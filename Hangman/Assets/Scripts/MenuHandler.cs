f0be2a08a2b4ecb9358675e917babc62dfd115d1
msllagas <94226891+msllagas@users.noreply.github.com>
2022-12-13 11:47:48 +0800

    Added Push Notification using Firebase Messaging
    Added Push Notification using Firebase Messaging

--- Hangman/Assets/Scripts/MenuHandler.cs
+++ Hangman/Assets/Scripts/MenuHandler.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 
+
 public class MenuHandler : MonoBehaviour
 {
     public bool isClicked = true;
@@ -29,6 +30,17 @@ public class MenuHandler : MonoBehaviour
     {
         UpdateStatsText();
         LoadBGMSession();
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        Debug.Log("Received a new message from: " + e.Message.From);
     }
 
     void UpdateStatsText()
